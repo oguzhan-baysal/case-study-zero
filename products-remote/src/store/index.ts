4f859431a782772a0f1a2ae00b6a4c4e6d5f318b
Oguzhan Baysal <110729148+oguzhan-baysal@users.noreply.github.com>
2025-03-18 18:24:30 +0300

    Add Redux store configuration

---
+++ products-remote/src/store/index.ts
@@ -0,0 +1,18 @@
+import { configureStore } from '@reduxjs/toolkit';
+import { setupListeners } from '@reduxjs/toolkit/query';
+import { productsApi } from '../services/api/products';
+import filtersReducer from './slices/filtersSlice';
+
+export const store = configureStore({
+  reducer: {
+    [productsApi.reducerPath]: productsApi.reducer,
+    filters: filtersReducer,
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(productsApi.middleware),
+});
+
+setupListeners(store.dispatch);
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
