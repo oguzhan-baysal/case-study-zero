f987d56c93b2ccc35397ff97a53a3760ab82265c
Oguzhan Baysal <110729148+oguzhan-baysal@users.noreply.github.com>
2025-03-18 18:06:53 +0300

    Add Basket component

--- basket-remote/src/components/Basket.tsx
+++ basket-remote/src/components/Basket.tsx
@@ -1,67 +1,83 @@
-import React from 'react'
-import { useSelector, useDispatch } from 'react-redux'
-import { Button, List, InputNumber, Typography, Card } from 'antd'
-import { DeleteOutlined } from '@ant-design/icons'
-import { RootState } from '../store/store'
-import { removeItem, updateQuantity } from '../store/basketSlice'
-import type { BasketItem as BasketItemType } from '../store/basketSlice'
+import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { Button, List, InputNumber, Typography, Card, Space } from 'antd';
+import { DeleteOutlined } from '@ant-design/icons';
+import { RootState } from '../store/provider';
+import { removeFromBasket, updateQuantity, clearBasket } from '../store/basketSlice';
 
-const { Title } = Typography
+const { Text } = Typography;
 
 const Basket: React.FC = () => {
-  const { items, total } = useSelector((state: RootState) => state.basket)
-  const dispatch = useDispatch()
-
-  const handleRemove = (id: number) => {
-    dispatch(removeItem(id))
-  }
+  const dispatch = useDispatch();
+  const { items, totalAmount } = useSelector((state: RootState) => state.basket);
 
   const handleQuantityChange = (id: number, quantity: number) => {
     if (quantity > 0) {
-      dispatch(updateQuantity({ id, quantity }))
+      dispatch(updateQuantity({ id, quantity }));
+    } else {
+      dispatch(removeFromBasket(id));
     }
+  };
+
+  const handleRemoveItem = (id: number) => {
+    dispatch(removeFromBasket(id));
+  };
+
+  const handleClearBasket = () => {
+    dispatch(clearBasket());
+  };
+
+  if (items.length === 0) {
+    return (
+      <Card>
+        <Text>Sepetiniz boş</Text>
+      </Card>
+    );
   }
 
-  const renderItem = (item: BasketItemType) => (
-    <List.Item
-      key={item.id}
-      actions={[
-        <InputNumber
-          key="quantity"
-          min={1}
-          value={item.quantity}
-          onChange={(value) => handleQuantityChange(item.id, value || 1)}
-        />,
-        <Button
-          key="remove"
-          type="text"
-          danger
-          icon={<DeleteOutlined />}
-          onClick={() => handleRemove(item.id)}
-        />
-      ]}
-    >
-      <List.Item.Meta
-        title={item.name}
-        description={`${item.price} TL`}
-      />
-      <div>{(item.price * item.quantity).toFixed(2)} TL</div>
-    </List.Item>
-  )
-
   return (
-    <Card title={<Title level={4}>Sepetim</Title>}>
+    <Card
+      title="Sepetim"
+      extra={
+        <Button type="link" danger onClick={handleClearBasket}>
+          Sepeti Temizle
+        </Button>
+      }
+    >
       <List
+        itemLayout="horizontal"
         dataSource={items}
-        renderItem={renderItem}
-        footer={
-          <div style={{ textAlign: 'right' }}>
-            <Title level={5}>Toplam: {total.toFixed(2)} TL</Title>
-          </div>
-        }
+        renderItem={(item) => (
+          <List.Item
+            actions={[
+              <Button
+                type="text"
+                danger
+                icon={<DeleteOutlined />}
+                onClick={() => handleRemoveItem(item.id)}
+              />,
+            ]}
+          >
+            <List.Item.Meta
+              title={item.name}
+              description={`Birim Fiyat: ${item.price.toFixed(2)} TL`}
+            />
+            <Space>
+              <InputNumber
+                min={0}
+                value={item.quantity}
+                onChange={(value) => handleQuantityChange(item.id, value || 0)}
+              />
+              <Text>{(item.price * item.quantity).toFixed(2)} TL</Text>
+            </Space>
+          </List.Item>
+        )}
       />
+      <div style={{ textAlign: 'right', marginTop: 16 }}>
+        <Text strong>Toplam: {totalAmount.toFixed(2)} TL</Text>
+      </div>
     </Card>
-  )
-}
+  );
+};
 
-export default Basket
+export default Basket;
