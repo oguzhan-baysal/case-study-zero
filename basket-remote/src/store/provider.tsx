b510ef508e17d3d2a3834b60f8c1d67569fd32c0
Oguzhan Baysal <110729148+oguzhan-baysal@users.noreply.github.com>
2025-03-18 18:06:39 +0300

    Add Redux provider configuration

--- basket-remote/src/store/provider.tsx
+++ basket-remote/src/store/provider.tsx
@@ -1,16 +1,20 @@
-import React from 'react'
-import { store } from './store'
-import { Provider } from 'react-redux'
-import { ReactNode } from 'react'
+import { configureStore } from '@reduxjs/toolkit';
+import { Provider } from 'react-redux';
+import basketReducer from './basketSlice';
+
+const store = configureStore({
+  reducer: {
+    basket: basketReducer,
+  },
+});
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
 
 interface ProvidersProps {
   children: React.ReactNode;
 }
 
-export const Providers = ({ children }: ProvidersProps) => {
-  return (
-    <Provider store={store}>
-      {children}
-    </Provider>
-  )
-}
+export const Providers: React.FC<ProvidersProps> = ({ children }) => {
+  return <Provider store={store}>{children}</Provider>;
+};
